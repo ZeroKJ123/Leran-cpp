8039561d1bc40988fbca586f7320f16ce972e0b6
Zero <valzorian.exe.com>
2024-07-14 12:40:40 +0300

    Friends)

--- main.cpp
+++ main.cpp
@@ -1,19 +1,50 @@
 #include <iostream>
 using namespace std;
 
-int Foo(int *pa)
+class Integer
 {
-    return (*pa)++;
+public:
+    Integer(int number)
+    {
+        value=number;
+    }
+    friend int compare(const Integer& obj1, const Integer& obj2);
+
+    void print() const
+    {
+        std::cout << "Value: " << value << std::endl;
+    }
+private:
+    int value;
+};
+
+int compare(const Integer& obj1, const Integer& obj2)
+{
+    return obj1.value - obj2.value;
+}
+
+void print(const Integer& num1, const Integer& num2)
+{
+    auto result = compare(num1, num2);
+    if(result > 0)
+    {
+        cout << "num1 greater than num2" << endl;
+    }
+    else if(result < 0)
+    {
+        cout << "num1 less than num2" << endl;
+    }
+    else
+    {
+        cout << "num1 equal than num2" << endl;
+    }
+
 }
 
 int main()
 {
-    int a = 0;
-
-    cout << a <<  endl;
-
-    Foo(&a);
-
-    cout << a <<  endl;
+    print(Integer{10}, Integer{15});
+    print(Integer{15}, Integer{10});
+    print(Integer{15}, Integer{15});
 }
 
